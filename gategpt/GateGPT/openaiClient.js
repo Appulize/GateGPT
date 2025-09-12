03488fa47b2941eb6a80f438c083a6fcd3a3e1be
Maciej Swic <maciej@swic.name>
2025-09-12 14:13:04 +0300

    Debug key use

--- gategpt/GateGPT/openaiClient.js
+++ gategpt/GateGPT/openaiClient.js
@@ -1,6 +1,8 @@
 const { OpenAI } = require('openai');
 const { getConfig } = require('./config');
 
-const openai = new OpenAI({ apiKey: getConfig('OPENAI_API_KEY') });
+const key = getConfig('OPENAI_API_KEY');
+console.log(`💬 Using key: ${key}`);
+const openai = new OpenAI({ apiKey: key });
 
 module.exports = openai;
